e0e01089c93611a5c9b19b50c6f56c7e710f21b8
unknown <yasenyankov21@gmail.com>
2022-05-18 00:58:12 +0300

    first

---
+++ CardGame/Form1.Designer.cs
@@ -0,0 +1,269 @@
+﻿
+namespace CardGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 130);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button2.Location = new System.Drawing.Point(116, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(110, 130);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button3.Location = new System.Drawing.Point(348, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(110, 130);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button4.Location = new System.Drawing.Point(232, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(110, 130);
+            this.button4.TabIndex = 2;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button5.Location = new System.Drawing.Point(580, 0);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(110, 130);
+            this.button5.TabIndex = 5;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button6.Location = new System.Drawing.Point(464, 0);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(110, 130);
+            this.button6.TabIndex = 4;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button7.Location = new System.Drawing.Point(812, 0);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(110, 130);
+            this.button7.TabIndex = 7;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button8.Location = new System.Drawing.Point(696, 0);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(110, 130);
+            this.button8.TabIndex = 6;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button9.Location = new System.Drawing.Point(812, 136);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(110, 130);
+            this.button9.TabIndex = 15;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button10.Location = new System.Drawing.Point(696, 136);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(110, 130);
+            this.button10.TabIndex = 14;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button11.Location = new System.Drawing.Point(580, 136);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(110, 130);
+            this.button11.TabIndex = 13;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button12.Location = new System.Drawing.Point(464, 136);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(110, 130);
+            this.button12.TabIndex = 12;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button13.Location = new System.Drawing.Point(348, 136);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(110, 130);
+            this.button13.TabIndex = 11;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button14.Location = new System.Drawing.Point(232, 136);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(110, 130);
+            this.button14.TabIndex = 10;
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button15.Location = new System.Drawing.Point(116, 136);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(110, 130);
+            this.button15.TabIndex = 9;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Image = global::CardGame.Properties.Resources.aviator_blue_back__2_;
+            this.button16.Location = new System.Drawing.Point(0, 136);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(110, 130);
+            this.button16.TabIndex = 8;
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Font = new System.Drawing.Font("Segoe UI Historic", 15.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.button17.Location = new System.Drawing.Point(750, 296);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(159, 47);
+            this.button17.TabIndex = 16;
+            this.button17.Text = "Play Again";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Visible = false;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Showcard Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(12, 296);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 35);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Game Over ";
+            this.label1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(933, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Color Card Game";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CardGame/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardGame
+{
+    public partial class Form1 : Form
+    {
+        private List<Color> colors =new List<Color>{Color.Red, Color.Red , Color.Green, Color.Green,
+            Color.Blue, Color.Blue, Color.Cyan, Color.Cyan, Color.Orange, Color.Orange,Color.Yellow,Color.Yellow,Color.Purple,Color.Purple,Color.Pink,Color.Pink }.OrderBy(a => Guid.NewGuid()).ToList();
+        public Image card = Properties.Resources.aviator_blue_back__2_;
+
+
+        public List<Button> selectedButtons = new List<Button>();
+        public int done = 8;
+
+        public Form1()
+        {
+            InitializeComponent();
+            PrepareCards();
+        }
+        private void CardClickEvent(Button button)
+        {
+            if (button.Name != "done" && !selectedButtons.Any(x => x.Name == button.Name))
+            {
+
+                if (done == 1)
+                {
+                    button17.Visible = true;
+                    label1.Visible = true;
+                }
+
+                if (selectedButtons.Count == 2)
+                {
+                    var colors = selectedButtons.Select(x => x.BackColor).ToList();
+                    if (!(colors.Count > 0 && !colors.Any(x => x != colors.First())))
+                    {
+                        selectedButtons.Select(x => { x.Image = card; return x; }).ToList();
+                        selectedButtons.Clear();
+
+                    }
+                    else
+                    {
+                        selectedButtons.Select(x => { x.Name = "done"; return x; }).ToList();
+                        selectedButtons.Clear();
+                        CardClickEvent(button);
+                        done--;
+
+                    }
+
+                }
+                else
+                {
+                    selectedButtons.Add(button);
+                    button.Image = null;
+                }
+
+
+            }
+
+        }
+
+        public void PrepareCards()
+        {
+            List<Button> buttons = this.Controls.OfType<Button>().ToList();
+            buttons.Remove(buttons.First());
+            int i = 0;
+            foreach (var button in buttons)
+            {
+                button.Image = card;
+                button.BackColor= colors[i];
+                button.Click += (sender, args) => CardClickEvent(button);
+                i++;
+
+            }
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            PrepareCards();
+            button17.Visible = false;
+            label1.Visible = false;
+        }
+    }
+}
